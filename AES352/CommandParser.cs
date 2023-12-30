9361e98ed4a036585432f199a23bc670397ddc3b
ga00ga <148340308+ga00ga@users.noreply.github.com>
2023-12-30 11:27:04 +0530

    quick fix

--- AES352/CommandParser.cs
+++ AES352/CommandParser.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 
 public class CommandParser
 {
@@ -10,21 +10,33 @@ public class CommandParser
     private PictureBox displayArea;
     private Graphics graphics;
     private Pen currentPen;
-    private PointF currentPosition;
+    public PointF currentPosition;
     private bool fillEnabled = false;
+
+    // Make properties public so they can be accessed by unit tests
+    public PointF CurrentPosition => currentPosition;
+    public Pen CurrentPen => currentPen;
+    public bool FillEnabled => fillEnabled;
 
     public CommandParser(TextBox codeTextBox, PictureBox displayArea)
     {
         this.codeTextBox = codeTextBox;
         this.displayArea = displayArea;
-        this.graphics = displayArea.CreateGraphics();
+
+        // Setup bitmap to draw on
+        Bitmap bmp = new Bitmap(displayArea.Width, displayArea.Height);
+        displayArea.Image = bmp;
+        this.graphics = Graphics.FromImage(bmp);
         this.currentPen = new Pen(Color.Black);
         this.currentPosition = new PointF(0, 0);
     }
 
-    public void ExecuteCommand(string command)
+    public void ExecuteProgram(string program)
+    {
+        var lines = codeTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
         {
-            var parts = command.Split(' ');
+            var parts = line.Trim().Split(' ');
             switch (parts[0].ToLower())
             {
                 case "moveto":
@@ -59,150 +71,187 @@ public class CommandParser
             }
         }
 
-        public void ExecuteProgram()
+        // After executing a command that draws something, refresh the PictureBox
+        displayArea.Invalidate();
+    }
+
+
+
+    public void ExecuteCommand(string command)
+    {
+        string[] lines = command.Split(' ');
+        switch (lines[0].ToLower())
         {
-            var commands = codeTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var command in commands)
+            case "moveto":
+                MoveTo(ParseFloat(lines[1]), ParseFloat(lines[2]));
+                break;
+            case "drawto":
+                DrawTo(ParseFloat(lines[1]), ParseFloat(lines[2]));
+                break;
+            case "clear":
+                Clear();
+                break;
+            case "rectangle":
+                DrawRectangle(ParseFloat(lines[1]), ParseFloat(lines[2]));
+                break;
+            case "circle":
+                DrawCircle(ParseFloat(lines[1]));
+                break;
+            case "triangle":
+                DrawTriangle(ParseFloat(lines[1]), ParseFloat(lines[2]), ParseFloat(lines[3]), ParseFloat(lines[4]), ParseFloat(lines[5]), ParseFloat(lines[6]));
+                break;
+            case "color":
+                SetColor(Color.FromName(lines[1]));
+                break;
+            case "reset":
+                ResetPenPosition();
+                break;
+            case "fill":
+                ToggleFill(lines[1]);
+                break;
+            default:
+                throw new ArgumentException($"Unknown command: {lines[0]}");
+        }
+        displayArea.Invalidate();
+    }
+
+    private float ParseFloat(string input)
+    {
+        if (float.TryParse(input, out float result))
+            return result;
+        throw new ArgumentException($"Unable to parse '{input}' as a float.");
+    }
+
+    public void SaveProgram(string filePath)
+    {
+        File.WriteAllText(filePath, codeTextBox.Text);
+    }
+
+    public void LoadProgram(string filePath)
+    {
+        codeTextBox.Text = File.ReadAllText(filePath);
+    }
+
+    public void CheckSyntax()
+    {
+        var commands = codeTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var command in commands)
+        {
+            if (!IsValidCommand(command.Trim()))
             {
-                ExecuteCommand(command.Trim());
+                MessageBox.Show($"Syntax error in command: {command}", "Syntax Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
         }
+        MessageBox.Show("All commands have valid syntax.", "Syntax Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+    private bool IsValidCommand(string command)
+    {
+        var lines = command.Split(' ');
+        var commandType = lines[0].ToLower();
 
-        public void SaveProgram(string filePath)
+        try
         {
-            File.WriteAllText(filePath, codeTextBox.Text);
-        }
-
-        public void LoadProgram(string filePath)
-        {
-            codeTextBox.Text = File.ReadAllText(filePath);
-        }
-
-        public void CheckSyntax()
-        {
-            var commands = codeTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var command in commands)
+            switch (commandType)
             {
-                if (!IsValidCommand(command))
-                {
-                    MessageBox.Show($"Syntax error in command: {command}", "Syntax Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-            }
-            MessageBox.Show("Syntax is correct!", "Syntax Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        private bool IsValidCommand(string command)
-        {
-            var parts = command.Split(' ');
-            var commandType = parts[0].ToLower();
-
-            try
-            {
-                switch (commandType)
-                {
-                    case "moveto":
-                    case "drawto":
-                        return parts.Length == 3 && parts.Skip(1).All(p => float.TryParse(p, out _));
-                    case "rectangle":
-                        return parts.Length == 5 && parts.Skip(1).All(p => float.TryParse(p, out _));
-                    case "circle":
-                        return parts.Length == 2 && float.TryParse(parts[1], out _);
-                    case "triangle":
-                        return parts.Length == 7 && parts.Skip(1).All(p => float.TryParse(p, out _));
-                    case "color":
-                        return parts.Length == 2;
-                    case "clear":
-                    case "reset":
-                        return parts.Length == 1;
-                    case "fill":
-                        return parts.Length == 2 && (parts[1].ToLower() == "on" || parts[1].ToLower() == "off");
-                    default:
-                        return false;
-                }
-            }
-            catch
-            {
-                return false;
+                case "moveto":
+                case "drawto":
+                    return lines.Length == 3 && lines.Skip(1).All(p => float.TryParse(p, out _));
+                case "rectangle":
+                    return lines.Length == 5 && lines.Skip(1).All(p => float.TryParse(p, out _));
+                case "circle":
+                    return lines.Length == 2 && float.TryParse(lines[1], out _);
+                case "triangle":
+                    return lines.Length == 7 && lines.Skip(1).All(p => float.TryParse(p, out _));
+                case "color":
+                    return lines.Length == 2 && Enum.IsDefined(typeof(KnownColor), lines[1]);
+                case "clear":
+                case "reset":
+                    return lines.Length == 1;
+                case "fill":
+                    return lines.Length == 2 && (lines[1].ToLower() == "on" || lines[1].ToLower() == "off");
+                default:
+                    return false;
             }
         }
-
-
-        private void MoveTo(float x, float y)
+        catch
         {
-            currentPosition = new PointF(x, y);
+            return false;
         }
+    }
 
-        private void DrawTo(float x, float y)
-        {
-            PointF newPosition = new PointF(x, y);
-            graphics.DrawLine(currentPen, currentPosition, newPosition);
-            currentPosition = newPosition;
-        }
+    private void MoveTo(float x, float y)
+    {
+        currentPosition = new PointF(x, y);
+    }
 
-        private void Clear()
-        {
-            graphics.Clear(displayArea.BackColor);
-            currentPosition = new PointF(0, 0);
-        }
+    private void DrawTo(float x, float y)
+    {
+        PointF newPosition = new PointF(x, y);
+        graphics.DrawLine(currentPen, currentPosition, newPosition);
+        currentPosition = newPosition;
+    }
 
-        private void DrawRectangle(float width, float height)
-        {
-            if (fillEnabled)
-                graphics.FillRectangle(currentPen.Brush, currentPosition.X, currentPosition.Y, width, height);
-            else
-                graphics.DrawRectangle(currentPen, currentPosition.X, currentPosition.Y, width, height);
-        }
+    private void Clear()
+    {
+        graphics.Clear(Color.White); 
+        currentPosition = new PointF(0, 0);
+    }
 
-        private void DrawCircle(float radius)
-        {
-            if (fillEnabled)
-                graphics.FillEllipse(currentPen.Brush, currentPosition.X - radius, currentPosition.Y - radius, radius * 2, radius * 2);
-            else
-                graphics.DrawEllipse(currentPen, currentPosition.X - radius, currentPosition.Y - radius, radius * 2, radius * 2);
-        }
+    private void DrawRectangle(float width, float height)
+    {
+        if (fillEnabled)
+            graphics.FillRectangle(currentPen.Brush, currentPosition.X, currentPosition.Y, width, height);
+        else
+            graphics.DrawRectangle(currentPen, currentPosition.X, currentPosition.Y, width, height);
+    }
 
-        private void DrawTriangle(float x1, float y1, float x2, float y2, float x3, float y3)
-        {
-            PointF[] points = { new PointF(x1, y1), new PointF(x2, y2), new PointF(x3, y3) };
-            if (fillEnabled)
-                graphics.FillPolygon(currentPen.Brush, points);
-            else
-                graphics.DrawPolygon(currentPen, points);
-        }
+    private void DrawCircle(float radius)
+    {
+        if (fillEnabled)
+            graphics.FillEllipse(currentPen.Brush, currentPosition.X - radius, currentPosition.Y - radius, radius * 2, radius * 2);
+        else
+            graphics.DrawEllipse(currentPen, currentPosition.X - radius, currentPosition.Y - radius, radius * 2, radius * 2);
+    }
 
-        private void SetColor(Color color)
-        {
-            currentPen.Color = color;
-        }
+    private void DrawTriangle(float x1, float y1, float x2, float y2, float x3, float y3)
+    {
+        PointF[] points = { new PointF(x1, y1), new PointF(x2, y2), new PointF(x3, y3) };
+        if (fillEnabled)
+            graphics.FillPolygon(currentPen.Brush, points);
+        else
+            graphics.DrawPolygon(currentPen, points);
+    }
 
-        private void ResetPenPosition()
-        {
-            currentPosition = new PointF(0, 0);
-        }
+    private void SetColor(Color color)
+    {
+        currentPen.Color = color;
+    }
 
-        private void ToggleFill(string state)
-        {
-            fillEnabled = state.ToLower() == "on";
-        }
+    private void ResetPenPosition()
+    {
+        currentPosition = new PointF(0, 0);
+    }
+
+    private void ToggleFill(string state)
+    {
+        fillEnabled = state.ToLower() == "on";
+    }
 
     public void SetupGraphics(PaintEventArgs e)
     {
         var g = e.Graphics;
-
         g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
         g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
         g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-
-
     }
 
-
     public void Cleanup()
-        {
+    {
         if (graphics != null)
             graphics.Dispose();
         if (currentPen != null)
             currentPen.Dispose();
     }
-    }
+
+}
